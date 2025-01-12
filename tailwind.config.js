a16ff0a8d9cad8fc2c498c8b33dc40945ff2afb4
BigNet90 <abubakaruzaifah@gmail.com>
2025-01-12 11:45:52 -0800

    edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./public/**/*.html", ".js", ".css",
-    "/public/index.html",
-=======
-  content: ["./public/**/*.html", ".js",".css",
     "/public/index.html", "/src/input.css",
->>>>>>> 216d12e8697e1ebb1503b3f338d9ee6dd20043bc
   ],
   theme: {
     extend: {},

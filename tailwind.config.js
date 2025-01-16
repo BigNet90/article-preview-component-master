31c042608f00502ec3d9e09d68748316a9e27b5d
BigNet90 <abubakaruzaifah@gmail.com>
2025-01-16 09:29:25 -0800

    merge new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   relative: true,
   content: ["./index.html",
     "./public/**/*.{html,js}"
   ],
-=======
-  content: ["/.public/**/*.{html,js}"],
->>>>>>> 35cf44b11d63a15b8b9322886937b5a7a55622fc
   theme: {
     extend: {},
   },
